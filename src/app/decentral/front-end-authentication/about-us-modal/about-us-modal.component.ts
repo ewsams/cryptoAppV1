7af6bd569826c13693e9f9bae5346ed8678b2df6
ewsams <edwardsams@email.arizona.edu>
2021-04-15 17:08:26 -0700

    added modal about us

--- src/app/decentral/front-end-authentication/about-us-modal/about-us-modal.component.ts
+++ src/app/decentral/front-end-authentication/about-us-modal/about-us-modal.component.ts
@@ -8,6 +8,7 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   template: `
   <div class="container" style="border:solid white 3px; position:absolute;top:0;"
   [ngStyle]="{'background-color': color ? 'rgb(121,141,179)' :'rgb(35,40,50)'}">
+  <div class="close-icon" (click)="closeModal()">X</div>
   <img
     src="../assets/img/appollo-logo.png"
     style="width: 6rem; height: 6rem;"
@@ -18,20 +19,20 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   />
   <span class="modal-header modal-header-style">
     <h3 class="modal-title" style="color:white;">
-      <strong style="border-bottom:solid white 1px;">
+      <strong class="large-message small-message">
         We aim to provide our Hodlers the best Liquidty and Lowest fees of any token</strong
       >
     </h3>
   </span>
   <div class="modal-body">
   <h3 style="text-align:center;font-family:Impact;">
-  Our goals through Quarter 4 2021 are as follows...</h3>
-  <h3 style="text-align:left; font-family:Times">I) Goal</h3>
-  <h3 style="text-align:left; font-family:Times">II) Goal</h3>
-  <h3 style="text-align:left; font-family:Times">III) Goal</h3>
-  <h3 style="text-align:left; font-family:Times">IV) Goal</h3>
-  <h3 style="text-align:left; font-family:Times">V) Goal</h3>
-  <h3 style="text-align:left; font-family:Times">VI) Goal</h3>
+  Our goals through Quarter 3 2021 are as follows...</h3>
+  <h3 style="text-align:left; font-family:Times">I) Complete our ICO.</h3>
+  <h3 style="text-align:left; font-family:Times">II) Begin bi-weekly lottos.</h3>
+  <h3 style="text-align:left; font-family:Times">III) Initialize our NFT market.</h3>
+  <h3 style="text-align:left; font-family:Times">IV) Use the master wallet to remove gas fees.</h3>
+  <h3 style="text-align:left; font-family:Times">V) Build a bridge in order to utilize other block chains.</h3>
+  <h3 style="text-align:left; font-family:Times">VI) Continue to build our presence and brand.</h3>
     <h4 style="text-align:center">"The world of crypto currency is at your
           fingertips"...</h4>
     </div>
@@ -42,7 +43,7 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class AboutUsModalComponent implements OnInit {
   color: boolean;
   colorSubscription: Subscription;
-  constructor( public activeModal: NgbActiveModal,
+  constructor( public activeModal: NgbModal,
                private userService: UserService) { }
 
   ngOnInit() {
@@ -53,6 +54,10 @@ export class AboutUsModalComponent implements OnInit {
 
    ngOnDestroy() {
      this.colorSubscription.unsubscribe();
+  }
+
+  closeModal() {
+    this.activeModal.dismissAll();
   }
 
 }
