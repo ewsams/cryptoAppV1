0282c2c0961e6839adf0e8056c086595d89d0f4a
ewsams <edwardsams@email.arizona.edu>
2021-06-04 17:42:15 -0700

    changes to spins

--- src/app/decentral/front-end-authentication/purchase-spins/purchase-spins.component.ts
+++ src/app/decentral/front-end-authentication/purchase-spins/purchase-spins.component.ts
@@ -3,89 +3,110 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
 import { Web3Service } from 'src/app/util/web3.service';
 import { UserService } from '../services/user.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FirestoreService } from '../services/firestore.service';
 
 @Component({
   selector: 'app-purchase-spins',
   templateUrl: './purchase-spins.component.html',
   styleUrls: ['./purchase-spins.component.scss']
 })
-export class PurchaseSpinsComponent implements OnInit {color: boolean;
+export class PurchaseSpinsComponent implements OnInit {
+  color: boolean;
   colorSubscription: Subscription;
   spinsTransactionReceipt: any;
   transactionSubscription: Subscription;
-  confirmationWallet:string;
-  appolloTransfered:number;
+  confirmationWallet: string;
+  appolloTransfered: number;
   spinsPurchased: string;
-  awaitingTransactionConfirmation:boolean;
+  awaitingTransactionConfirmation: boolean;
+  users$: any;
+  currentUser: any;
+  userSub: Subscription;
+  user: any;
+  userRetrieved: boolean;
 
-  
-  constructor( public activeModal: NgbModal,
-               private userService: UserService,
-               private web3Service: Web3Service) { }
+
+  constructor(public activeModal: NgbModal,
+    private userService: UserService,
+    private web3Service: Web3Service,
+    private afAuth: AngularFireAuth,
+    private db: FirestoreService) { }
 
   ngOnInit() {
+    this.userRetrieved = false;
     this.colorSubscription = this.userService.userBackgroundSelectionObservable$.subscribe(color => {
       this.color = color;
     });
-   }
+    this.getCurrentUser();
+  }
 
-   
-    purchase50Spins = async () => {
-      this.awaitingTransactionConfirmation = true;
-      await this.web3Service.lotteryDeposit(100000000000000000000);
-      this.confirmSpinsPurchase();
-    }
 
-    purchase100Spins = async () => {
-      this.awaitingTransactionConfirmation = true;
-      await this.web3Service.lotteryDeposit(250000000000000000000);
-      this.confirmSpinsPurchase();
-    }
+  purchase50Spins = async () => {
+    this.awaitingTransactionConfirmation = true;
+    await this.web3Service.lotteryDeposit(100000000000000000000);
+    this.confirmSpinsPurchase();
+  }
 
-    purchase200Spins = async () => {
-      this.awaitingTransactionConfirmation = true;
-      await this.web3Service.lotteryDeposit(500000000000000000000);
-      this.confirmSpinsPurchase();
-    }
+  purchase100Spins = async () => {
+    this.awaitingTransactionConfirmation = true;
+    await this.web3Service.lotteryDeposit(250000000000000000000);
+    this.confirmSpinsPurchase();
+  }
 
-    confirmSpinsPurchase = () => {
-      this.transactionSubscription = this.web3Service.spinsTransactionReceipt$.subscribe(receipt => { 
-          if(receipt.status === true){
-            this.spinsTransactionReceipt = receipt;
-            this.awaitingTransactionConfirmation = false;
-            
-            this.confirmationWallet = 
-            this.spinsTransactionReceipt.events.Transfer.address;
-            
-            this.appolloTransfered = 
-           Number(this.spinsTransactionReceipt.events.Transfer.returnValues.value) / 1000000000000000000;
-          }
-          if(this.appolloTransfered === 100){
-            this.spinsPurchased =
-            `Congrats you just purchased 50 spins using ${this.appolloTransfered} APP`;
-          }        
-          if(this.appolloTransfered === 250){
-            this.spinsPurchased = 
-            `Congrats you just purchased 100 spins using ${this.appolloTransfered} APP`;
-          }
-          if(this.appolloTransfered === 500){
-            this.spinsPurchased = 
-            `Congrats you just purchased 200 spins using ${this.appolloTransfered} APP`;
-          }
-      });  
-        console.log(this.spinsTransactionReceipt,this.confirmationWallet,this.appolloTransfered);
-    }
+  purchase200Spins = async () => {
+    this.awaitingTransactionConfirmation = true;
+    await this.web3Service.lotteryDeposit(500000000000000000000);
+    this.confirmSpinsPurchase();
+  }
 
-   ngOnDestroy() {
-     this.colorSubscription.unsubscribe();
-     if(this.transactionSubscription){
+  confirmSpinsPurchase = () => {
+    this.transactionSubscription = this.web3Service.spinsTransactionReceipt$.subscribe(receipt => {
+      if (receipt.status === true) {
+        this.spinsTransactionReceipt = receipt;
+        this.awaitingTransactionConfirmation = false;
+
+        this.confirmationWallet =
+          this.spinsTransactionReceipt.events.Transfer.address;
+
+        this.appolloTransfered =
+          Number(this.spinsTransactionReceipt.events.Transfer.returnValues.value) / 1000000000000000000;
+      }
+      if (this.appolloTransfered === 100) {
+        this.spinsPurchased =
+          `Congrats you just purchased 50 spins using ${this.appolloTransfered} APP`;
+      }
+      if (this.appolloTransfered === 250) {
+        this.spinsPurchased =
+          `Congrats you just purchased 100 spins using ${this.appolloTransfered} APP`;
+      }
+      if (this.appolloTransfered === 500) {
+        this.spinsPurchased =
+          `Congrats you just purchased 200 spins using ${this.appolloTransfered} APP`;
+      }
+    });
+    console.log(this.spinsTransactionReceipt, this.confirmationWallet, this.appolloTransfered);
+  }
+
+  ngOnDestroy() {
+    this.colorSubscription.unsubscribe();
+    if (this.transactionSubscription && this.userSub) {
       this.transactionSubscription.unsubscribe();
-     }
+      this.userSub.unsubscribe();
+    }
   }
 
   closeModal() {
     this.activeModal.dismissAll();
   }
 
+  getCurrentUser = async () => {
+    this.currentUser = await this.afAuth.currentUser;
+    this.userSub = this.db.colWithIds$('users').subscribe(users => {
+      this.users$ = users;
+      this.user = this.users$.find(user => user.email === this.currentUser.email);
+      this.userRetrieved = true;
+      console.log(this.users$);
+    });
+  }
 }
-
