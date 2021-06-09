677403bf0e3ebb01c25027092fa6867899f46769
ewsams <edwardsams@email.arizona.edu>
2021-06-09 10:09:10 -0700

    intial commit

--- src/app/decentral/front-end-authentication/loterry/loterry.component.ts
+++ src/app/decentral/front-end-authentication/loterry/loterry.component.ts
@@ -31,7 +31,8 @@ export class LoterryComponent implements OnInit {
   userRetrieved: boolean;
   alertMessageString: string;
 
-  constructor(private web3Service: Web3Service,
+  constructor(
+    private web3Service: Web3Service,
     private lottorryService: LotteryService,
     private modal: NgbModal,
     private userService: UserService,
@@ -112,6 +113,9 @@ export class LoterryComponent implements OnInit {
       }
     }
     this.web3Service.payLotteryWinner();
+    this.db.update(`profiles/${this.user.id}`, {
+      winner:true, amount:this.currentLotteryBalance
+    });
     return this.winnerString = `Congratulations You Win!`;
   }
 
